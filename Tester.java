eb7c0755f3f0feb1f2d9869bf91b3ebef5f7d910
rengzhengcodes <rzhengcontacts@gmail.com>
2020-11-01 03:58:07 -0500

    created tester for add(), get(), size(). Tested for #1 and 3. Passed

--- Tester.java
+++ Tester.java
@@ -4,27 +4,33 @@ public class Tester {
     System.out.println("SuperArray Tester");
     boolean failure = false;
     failure = constructorTester() || failure;
+    failure = addAndGetAndSizeTester() || failure;
+
+    if (failure) {
+      System.out.println("Is that blue smoke?");
+    } else {
+      System.out.println("All's good in the neighborhood");
+    }
   }
 
   private static void passMessage (int testCase) {
     System.out.println("Test case " + testCase + " passed.");
   }
 
-  private static void errorMessage(String expected, String actual) {
-    System.out.println("WE HAVE FAILED");
+  private static void errorMessage(int testCase, String expected, String actual) {
+    System.out.println("WE HAVE FAILED AT TEST CASE " + testCase);
     System.out.println("EXPECTED: " + expected);
     System.out.println("ACTUAL: " + actual);
   }
 
   public static boolean constructorTester() {
     System.out.println("\n ~~~ constructor TESTER ~~~");
-
     boolean failure = false;
     SuperArray test = new SuperArray();
-    if (test.size() == 10) {
+    if (test.size() == 0) {
       passMessage(0);
     } else {
-      errorMessage("" + 10, "" + test.size());
+      errorMessage(0, "" + 0, "" + test.size());
     }
 
     if (failure) {
@@ -32,6 +38,29 @@ public class Tester {
     } else {
       System.out.println("constructorTester() PASSED");
     }
+    return failure;
+  }
+
+  public static boolean addAndGetAndSizeTester() {
+    System.out.println("\nadd() AND get() AND size() TESTER");
+    boolean failure = false;
+    SuperArray test = new SuperArray();
+    String[] elementsToAdd = {
+      "foo",
+      "bar",
+      "bread"
+    };
+
+    System.out.println("add() + size() Portion of Testing");
+    for (int index = 0; index < elementsToAdd.length; index++) {
+      test.add(elementsToAdd[index]);
+      if (test.size() == index + 1) {
+        passMessage(index);
+      } else {
+        errorMessage(index, "" + (index + 1), "" + test.size());
+      }
+    }
+
     return failure;
   }
 
